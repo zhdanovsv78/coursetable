33a219bb5c4ad9966160cc9d7f45170bd390cb59
Sergey Zhdanov <zhsv007@yandex.ru>
2023-03-13 21:16:32 +0400

    update course

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -17,31 +17,37 @@ const Users=()=>{
 	}
     // Обработка события нажатия кнопки delete
     const handleDelete = (userId) => {
-        setUsers(prevState=>prevState.filter(user=>user._id!==userId))
+        setUsers(users.filter(user=>user._id!==userId))
     }
+
+	const resetTable = () => {
+		
+		users.setState({
+			user: [{}]
+		  });
+	}
 
     // Вывод правильного склонения фразы
     const renderPhrase = (number) => {
-        let phrase = "человек"
-        if ([2,3,4].includes(number)) {
-           phrase = "человека"
-        }
+        const phrase=([2,3,4].includes(number))?"человека":"человек"
         return phrase
     }
 
     // Вывод фразы в заголовке
     const titlePhrase = () => {
-        return (
-            <div className="badge bg-primary m-1 p-1 fs-5" style={stylePhrase}>
-                {users.length} {renderPhrase(users.length)} тусанет с тобой сегодня
-            </div>
-        )
+		const res = (users.length===0?"Никто с тобой не тусанет сегодня":(users.length+" "+renderPhrase(users.length)+" тусанет с тобой сегодня"))
+		return res
     }
 	
     return(
         <>
-        {titlePhrase()}
-		<table class="table">
+
+		<div className={"badge m-1 p-1 fs-5 bg-"+(users.length>0?"primary":"danger")} style={stylePhrase}>
+			{titlePhrase()}
+		</div>
+        
+		{users.length>0&&
+		<table className="table">
 			<thead>
 				<tr>
 					<th scope="col">Имя</th>
@@ -55,47 +61,48 @@ const Users=()=>{
 
 			<tbody>
 				{
-					users.map((user, index) => 
-					<tr key={user._id}>
-						{/* Имя */}
-						<th scope="row">{user.name}</th>
-						{/* Качества */}
-						<td>
-							{
-							user.qualities.map((item) => (
-								<div key = {item["_id"]} style={styleDiv} className={`badge bg-${item["color"]} m-1`}>{item["name"]}</div>
-							))
-							}
-						</td>
-						{/* Профессия */}
-						<td>
-							{
-							<div>{user.profession["name"]}</div>
-							}
-						</td>
-						{/* Встретился, раз */}
-						<td>
-							{
-							<div>{user.completedMeetings}</div>
-							}
-						</td>
-						{/* Оценка */}
-						<td>
-							{
-							<div>{user.rate}/5</div>
-							}
-						</td>
-						{/* Кнопка удалить */}
-						<td>
-							{
-							<button className="btn btn-danger" onClick={()=>handleDelete(user._id)}>delete</button>
-							}
-						</td>
-					</tr>
+					users.map((user) => 
+						<tr key={user._id}>
+							{/* Имя */}
+							<th scope="row">{user.name}</th>
+
+							{/* Качества */}
+							<td>
+								{
+								user.qualities.map((item) => (
+									<div key = {item._id} style={styleDiv} className={`badge bg-${item.color} m-1`}>{item.name}</div>
+								))
+								}
+							</td>
+							{/* Профессия */}
+							<td>
+								{
+								<div>{user.profession.name}</div>
+								}
+							</td>
+							{/* Встретился, раз */}
+							<td>
+								{
+								<div>{user.completedMeetings}</div>
+								}
+							</td>
+							{/* Оценка */}
+							<td>
+								{
+								<div>{user.rate}/5</div>
+								}
+							</td>
+							{/* Кнопка удалить */}
+							<td>
+								{
+								<button className={"btn btn-danger"} onClick={()=>handleDelete(user._id)}>delete</button>
+								}
+							</td>
+						</tr>
 					)
 				}
 			</tbody>
-		</table>
+		</table>}
         </>
     )
 }
